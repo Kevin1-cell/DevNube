5de9d3f53042aa5119e364382a7aa36d5ed35e7d
Kevin Santiago Aguirre <devnubesa@gmail.com>
2025-08-26 08:27:45 +0000

    Change made to Json-Scripts for docker problems

--- server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get('/', (req, res) => res.send('¡Hola desde Express en Kubernetes Desde powerShel, Prueba 3!'));
-app.get('/healthz', (req, res) => res.status(200).send('ok'));
-app.get('/ready', (req, res) => res.status(200).send('ready'));
-
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
